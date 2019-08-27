02ca5987a1aa7b352b491e37af683b20d597fc83
s-malchenko <malchenko.sg@gmail.com>
2019-08-27 13:10:55 +0300

    CMake project created

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        cout << argv[1] << endl;
+    }
+
+    cout << "Block hasher is ready to be implemented!" << endl;
+    return 0;
+}
